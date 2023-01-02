4015a86fa783950ecef3b2fc361dee55e0e98dac
Vladimír Suja <77164934+maf1an@users.noreply.github.com>
2023-01-02 22:29:04 +0100

    Dokončenie počítadla

--- fps/script/HUD.gd
+++ fps/script/HUD.gd
@@ -1,10 +1,16 @@
 extends Control
-
-
+var premenna_jablk: int = 0
+var nic = true
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	get_node("ColorRect/Label").text = str()
+	$ColorRect/Label.text = str(premenna_jablk)
+#	_on_apple_pocet(premenna_jablk)
+#	print(premenna_jablk)
+#	get_node("ColorRect/Label").text = str(premenna_jablk)
+#	print(premenne.a)
+func _on_apple_pocet(value):
+	premenna_jablk += value
+	$ColorRect/Label.text = str(premenna_jablk)
--- fps/script/apple.gd
+++ fps/script/apple.gd
@@ -1,9 +1,9 @@
 extends Area
+#onready var jablko = 0
+signal pocet(value)
 
-onready var jablko = 0
 func _on_apple_body_entered(body):
 	if body.get_name() == "Player":
-		jablko += 1
-		print(jablko)
+		#premenne.add_score()
+		emit_signal("pocet", 1)
 		queue_free()
-		
