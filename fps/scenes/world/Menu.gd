187cd1a437c877a5f814b5ab254bb09893cf5f25
Vladimír Suja <77164934+maf1an@users.noreply.github.com>
2023-01-04 17:50:32 +0100

    Robenie levelu a pridanie Options možnosť

--- fps/scenes/world/Menu.gd
+++ fps/scenes/world/Menu.gd
@@ -23,3 +23,7 @@ func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/world/World.tscn")
 func _on_Button3_pressed():
 	get_tree().quit()
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://scenes/world/Options.tscn")
